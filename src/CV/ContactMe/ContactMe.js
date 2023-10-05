78b822659d04ae867e17aa296603880fcefb4c31
Muteman30 <jameshyde30@gmail.com>
2023-10-05 18:00:42 +0100

    Amend link

--- src/CV/ContactMe/ContactMe.js
+++ src/CV/ContactMe/ContactMe.js
@@ -6,7 +6,7 @@ const ContactMe = (props)=>{
     return(
         <CVSection>
             <Typography variant="h3">Contact Me</Typography>
-            <Typography variant="body">Thanks for taking the time to read about me! If you'd like to get in touch about anything you can find me on <a href="https://www.linkedin.com/in/james-hyde-565466141/" target="_blank">LinkedIn</a> or Github</Typography>
+            <Typography variant="body">Thanks for taking the time to read about me! If you'd like to get in touch about anything you can find me on <a href="https://www.linkedin.com/in/james-hyde-565466141/" target="_blank" rel="noreferrer">LinkedIn</a> or Github</Typography>
         </CVSection>
     )
 }
